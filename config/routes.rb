28d0c0e0c17765471b7b36c5d68612d6eb8d6bc4
tom apted <tapted0@gmail.com>
2023-02-28 11:52:06 +0000

    made relevent workspaces methods and views

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :workspaces, only: [:index, :show] do
+  resources :workspaces do
     resources :bookings, only: [:new, :create]
   end
   resources :bookings, only: [:destroy]
