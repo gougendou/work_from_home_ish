8622a54503424dffc40924ccef1e292c577cba7c
SamTreon <stat123@hotmail.co.uk>
2023-02-28 15:32:30 +0000

    please let this fix - changed something in the javascript index file

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,7 +2,7 @@
 // Run that command whenever you add a new controller or create them with
 // ./bin/rails generate stimulus controllerName
 
-import { application } from "./application"
+import { application } from "controller/application"
 
-import HelloController from "./hello_controller"
+import HelloController from "name/hello_controller"
 application.register("hello", HelloController)
