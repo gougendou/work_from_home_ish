5411aafc52ffc55d3b1cac2fda5e36d066513ed4
SamTreon <stat123@hotmail.co.uk>
2023-03-02 15:28:59 +0000

    added the full favourite functionality on the workspace index and favourites index

--- app/controllers/favourites_controller.rb
+++ app/controllers/favourites_controller.rb
@@ -4,29 +4,19 @@ class FavouritesController < ApplicationController
     @favourites = current_user.favourites
   end
 
-  #   # def create
-  #   #   @workspace = Workspace.find(params[:workspace_id])
-  #   #   @booking = Booking.new(strong_params)
-  #   #   @booking.workspace = @workspace
-  #   #   @booking.user = current_user
-  #   #   if @booking.save
-  #   #     redirect_to bookings_path
-  #   #   else
-  #   #     render :new, status: :unprocessable_entity
-  #   #   end
-  #   # end
+    def create
+      @favourite = Favourite.create(workspace_id: params[:workspace_id], user_id: current_user.id)
+      redirect_to workspaces_path
+    end
 
     def destroy
       @favourite = Favourite.find(params[:id])
       @favourite.destroy
-      redirect_to favourites_path, status: :see_other
+      redirect_back fallback_location: workspaces_path
     end
 
     # private
 
-    # def strong_params
-    #   params.require(:booking).permit(:start_date, :end_date)
-    # end
 
 
 end
