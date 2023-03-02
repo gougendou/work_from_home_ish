7b4fca60894908e3686fd5bb2d21205a46835848
SamTreon <stat123@hotmail.co.uk>
2023-03-02 12:33:01 +0000

    favorite typo

--- app/models/workspace.rb
+++ app/models/workspace.rb
@@ -1,6 +1,6 @@
 class Workspace < ApplicationRecord
   has_many :bookings, dependent: :destroy
-  has_many :favorites, dependent: :destroy
+  has_many :favourites, dependent: :destroy
   belongs_to :user
   has_many_attached :photos
   geocoded_by :address
